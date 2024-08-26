6da188e343d5f1ee0d6ccbcefb011a17f829b8b9
Gauravkhatiwada <132439835+Gauravkhatiwada@users.noreply.github.com>
2024-08-26 22:58:12 +0545

    Update Header.tsx

--- src/components/non-authenticted/Header.tsx
+++ src/components/non-authenticted/Header.tsx
@@ -130,7 +130,7 @@ const Header: React.FC = () => {
             />
             <div className="mt-4 px-3">
               <h1 className="text-xs font-bold sm:text-2xl primary-navy-blue">
-                MSP ACADEMY
+                gk ACADEMY
               </h1>
             </div>
           </div>
